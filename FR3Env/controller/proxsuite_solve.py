5ef6038933e9da59047dc5080bd4ef91cd7bf8e5
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2022-11-04 22:25:21 -0400

    added proxsuite solver (does not work yet)

---
+++ FR3Env/controller/proxsuite_solve.py
@@ -0,0 +1,47 @@
+import numpy as np
+import proxsuite
+
+
+class ProxsuiteSolver:
+    def __init__(self, nq):
+        self.n = nq
+        self.n_eq = 0
+        self.n_ieq = nq
+        self.qp = proxsuite.proxqp.dense.QP(self.n, self.n_eq, self.n_ieq)
+        self.initialized = False
+
+    def solve(self, params):
+        H, g, A, b, C, u, l = self.compute_params(params)
+
+        if not self.initialized:
+            self.qp.init(H, g, A, b, C, l, u)
+            self.initialized = True
+        else:
+            self.qp.update(H, g, A, b, C, l, u)
+
+        self.qp.solve()
+
+    def compute_params(self, params):
+        H = 2 * (
+            params["Jacobian"].T @ params["Jacobian"]
+            + params["nullspace_proj"].T @ params["nullspace_proj"]
+        )
+
+        g = (
+            -2
+            * (
+                params["q_measured"].T @ params["Jacobian"].T @ params["Jacobian"]
+                + params["p_error"].T @ params["Jacobian"]
+                + params["q_nominal"].T
+                @ params["nullspace_proj"].T
+                @ params["nullspace_proj"]
+            )[0, :]
+        )
+
+        A = np.zeros((self.n_eq, 9))
+        b = np.zeros((self.n_eq,))
+        C = np.eye(self.n_ieq)
+        u = params["q_max"][:, 0]
+        l = params["q_min"][:, 0]
+
+        return H, g, A, b, C, l, u
