2aedb3d27b563ab413541d1d0c94e5d83d6cfa3c
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-11-08 14:05:29 -0500

    updated mj diff ik example

--- examples/hello_world_mj_diff_ik.py
+++ examples/hello_world_mj_diff_ik.py
@@ -12,47 +12,48 @@ def main():
     env = FR3MuJocoEnv()
     info = env.reset()
 
-    T_init = pin.SE3(info["R_EE"], info["P_EE"])
-    # R_end = pin.rpy.rpyToMatrix(0.0, 0.0, 0.0)
-    # p_end = np.array([[0.3], [0.0], [0.5]])
-    # T_end = pin.SE3(R_end, p_end)
-    T_end = pin.SE3(info["R_EE"], info["P_EE"])
+    T_init = pin.SE3(info["R_EE"].copy(), info["P_EE"].copy())
+    p_end = np.array([[0.4], [0.3], [0.3]])
+    T_end = pin.SE3(info["R_EE"].copy(), p_end)
     t_init = 0.0
-    t_end = 10.0
+    t_end = 5.0
     curve = SE3Curve(T_init, T_end, t_init, t_end)
 
     controller = DiffIK()
 
-    for i in range(10000):
+    for i in range(12000):
         if not env.viewer.is_running():
             break
 
-        T = pin.SE3(info["R_EE"], info["P_EE"])
+        T = pin.SE3(info["R_EE"].copy(), info["P_EE"].copy())
 
-        t = i * env.model.opt.timestep
+        t = np.clip(i * 2e-3, 0.0, t_end)
 
-        if t <= t_end:
-            V = pin.log6(T.inverse().homogeneous @ curve(t)).vector
-            V_des = 1.0 * V + curve.derivate(t, 1)
-        else:
-            # V = pin.log6(T.inverse().homogeneous @ T_end).vector
-            # V_des = 1.0 * V + np.zeros((6,))
-            V_des = np.zeros((6,))
+        v_error = pin.log6(curve(t) @ T.inverse().homogeneous).vector
+        v_des = curve.derivate(t, 1) + 1.0 * v_error
+        Δdq = controller(v_des, info["q"], info["J_EE"])
 
-        dq_des = controller(V_des, info["q"], info["J_EE"])
-        q_des = info["q"] + dq_des * env.model.opt.timestep
-
-        # print(dq_des[:7])
-
-        _tau = (
-            1.0 * (q_des[:7] - info["q"][:7])
-            + 0.1 * (dq_des[:7] - info["dq"][:7])
-            + info["G"][:7]
-        )
+        _tau = 10.0 * Δdq + info["G"] - 6.0 * info["dq"]
         tau, finger_pos = _tau[:7], 0.0
 
         info = env.step(tau, finger_pos)
         time.sleep(1e-3)
+
+        if i == 3300:
+            T_init = pin.SE3(info["R_EE"].copy(), info["P_EE"].copy())
+            p_end = np.array([[0.4], [-0.3], [0.3]])
+            T_end = pin.SE3(info["R_EE"].copy(), p_end)
+            t_init = t
+            t_end = t + 6.0
+            curve = SE3Curve(T_init, T_end, t_init, t_end)
+
+        if i == 6600:
+            T_init = pin.SE3(info["R_EE"].copy(), info["P_EE"].copy())
+            p_end = np.array([[0.3], [0.0], [0.5]])
+            T_end = pin.SE3(info["R_EE"].copy(), p_end)
+            t_init = t
+            t_end = t + 5.0
+            curve = SE3Curve(T_init, T_end, t_init, t_end)
 
     env.close()
 
